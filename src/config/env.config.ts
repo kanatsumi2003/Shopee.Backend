7a17fd1ecb9eba171e38b203d6bcc53df7c20f5b
kanatsumi2003 <naduccva@gmail.com>
2025-07-03 14:21:01 +0700

    create empty .env

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
-if (NODE_ENV !== "production") {
+if (NODE_ENV === "production") {
+    // Only load .env
+    dotenv.config({ path: path.resolve(process.cwd(), `.env.production.deployment`) });
+    console.log("[ENV] Loaded .env.production.deployment for production");
+} else {
     // Load environment-specific local file: .env.development.local, .env.test.local, etc.
     const envPath = path.resolve(process.cwd(), `.env.${NODE_ENV}.local`);
     dotenv.config({ path: envPath });
     console.log(`[ENV] Loaded ${envPath}`);
-    // Only load .env
-} else {
-    console.log("[ENV] Loaded env for production with docker env");
 }
